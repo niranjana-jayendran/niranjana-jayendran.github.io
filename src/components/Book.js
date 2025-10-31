98db992745575e47ba25ff63d5d00027e46e0b8f
niranjana-jayendran <ninijai@outlook.com>
2025-10-31 06:45:47 -0400

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Book.css';
 import GeneralPage from './pages/GeneralPage';
 import BusinessPage from './pages/BusinessPage';
